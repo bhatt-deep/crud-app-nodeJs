26817daf59b39ce33171a59857009c6e54b09844
Deep Bhatt <bhatt.deep.001@gmail.com>
2020-12-30 04:37:13 -0500

    Changes made as per required after review

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 import express from 'express'
 import entriesRoutes from './src/entries.js'
 import usersRoutes from './src/users.js'
+import bodyParser from 'body-parser'
 const app = express()
 const port = 3000
 const fs = require('fs');
+// parse requests of content-type - application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }))
 
 // allows us to parse json 
 app.use(express.json())
