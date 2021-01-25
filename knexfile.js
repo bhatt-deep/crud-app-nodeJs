55393b062c3501bce1939e8fa9bfae690825a392
Deep Bhatt <bhatt.deep.001@gmail.com>
2021-01-25 03:35:54 -0500

    updated KNEX

--- knexfile.js
+++ knexfile.js
@@ -4,10 +4,10 @@ module.exports = {
   
   client: 'mysql2',
   connection: {
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
+    host: process.env.DATABASE_HOST,
+    user: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE_NAME,
   }
   
 };
