70a4ab9b7c28eebc53cb2e534824fb4c1942c801
Deep Bhatt <bhatt.deep.001@gmail.com>
2021-02-08 01:36:42 -0500

    implemented CD setup

--- knexfile.js
+++ knexfile.js
@@ -1,13 +1,21 @@
 // Update with your config settings.
 require('dotenv').config();
+
+let connection = {
+  user : process.env.DATABASE_USER,
+  password : process.env.DATABASE_PASSWORD,
+  database : process.env.DATABASE_NAME
+}
+if (process.env.DATABASE_SOCKET) {
+  connection.socketPath = process.env.DATABASE_SOCKET
+} else {
+  connection.host = process.env.DATABASE_HOST
+}
+console.log(connection)
 module.exports = {
-  
-  client: 'mysql2',
-  connection: {
-    host: process.env.DATABASE_HOST,
-    user: process.env.DATABASE_USER,
-    password: process.env.DATABASE_PASSWORD,
-    database: process.env.DATABASE_NAME,
+      client: 'mysql',
+      connection,
+      migrations: {
+        tableName: 'knex_migrations'
+      }
   }
-  
-};
